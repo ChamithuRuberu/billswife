f63072c4ff3ada6b8d313448d949e766b4c8e35b
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2024-10-07 03:00:10 +0530

    nav updated

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customRed: '#BC512F', // Custom color name
+        },
+      },
+    },
+  }
+  
