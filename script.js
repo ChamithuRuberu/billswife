c03c676f284ad4f3bfde987d8b66e8eee40d563b
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2024-10-07 21:03:16 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobileMenu');
+// const hamburger = document.getElementById('hamburger');
+// const mobileMenu = document.getElementById('mobileMenu');
 
-hamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
+// hamburger.addEventListener('click', () => {
+//     mobileMenu.classList.toggle('hidden');
+// });
+
+
+
+//     document.getElementById('aboutLink').addEventListener('click', function () {
+//         document.getElementById('bottom-section').scrollIntoView({ behavior: 'smooth' });
+//     });
+
+//     document.getElementById('contractLink').addEventListener('click', function () {
+//         document.getElementById('bottom-section').scrollIntoView({ behavior: 'smooth' });
+//     });
+
+//     document.getElementById('buyLink').addEventListener('click', function () {
+//         document.getElementById('bottom-section').scrollIntoView({ behavior: 'smooth' });
+//     });
